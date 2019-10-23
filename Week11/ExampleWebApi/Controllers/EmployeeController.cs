b7684c272f553135a060141f3dfb7e4d95ecdb97
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-10-23 11:00:34 +1100

    Completed GET, Get by ID, Put, POST and Delete

---
+++ Week11/ExampleWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+
+namespace ExampleWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        static string testvalue = "Julian";
+        static string testvalue2 = "Assange";
+        static int testvalue3 = 4;
+
+        // GET: api/Employee
+        public IEnumerable<string> Get()
+        {
+
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd;
+            SqlDataReader rdr;
+            String query;
+            List<String> output = new List<string>();
+
+            try
+            {
+                conn.Open();
+
+
+                query = "select * from employee";
+                cmd = new SqlCommand(query, conn);
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    output.Add("{staffid: " + rdr.GetValue(0)
+                                 + ", givenname: \"" + rdr.GetValue(1) + "\""
+                                 + ", surname: \"" + rdr.GetValue(2) + "\"}");
+                }
+
+            }
+            catch(Exception e)
+            {
+                output.Clear();
+                output.Add(e.Message);
+                
+            }
+            finally
+            {
+                if(conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+            return output;
+        }
+
+        // GET: api/Employee/5
+        public string Get(int id)
+        {
+
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd;
+            SqlDataReader rdr;
+            String query;
+            String output = "";
+
+            try
+            {
+                conn.Open();
+
+
+                query = "select * from employee where staffid =" + id;
+                cmd = new SqlCommand(query, conn);
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    output = output + "{staffid: " + rdr.GetValue(0)
+                                 + ", givenname: \"" + rdr.GetValue(1) + "\""
+                                 + ", surname: \"" + rdr.GetValue(2) + "\"}";
+                }
+
+            }
+            catch (Exception e)
+            {
+                output = "";
+                output = output + e.Message;
+
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+            return output;
+        }
+
+        // POST: api/Employee
+        public void Post([FromBody]string value)
+        {
+
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd;
+            SqlDataAdapter rda;
+            String query;
+            String output = "";
+
+            try
+            {
+                conn.Open();
+
+
+                query = "Insert into employee (staffid,givenname,surname) values('" + testvalue3 + "', '" + testvalue + "'," + testvalue2 + "' )";
+                cmd = new SqlCommand(query, conn);
+                rda = new SqlDataAdapter();
+
+                rda.InsertCommand = new SqlCommand(query, conn);
+                rda.InsertCommand.ExecuteNonQuery();
+
+
+            }
+            catch (Exception e)
+            {
+                output = "";
+                output = output + e.Message;
+
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+        }
+
+        // PUT: api/Employee/5
+        public void Put(int id, [FromBody]string value)
+        {
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd;
+            SqlDataAdapter rda;
+            String query;
+
+            string output = "";
+
+            try
+            {
+                conn.Open();
+
+
+                query = "Update employee set givenname='" + testvalue + "', surname='" + testvalue2 + "' where staffid=" +id;
+                cmd = new SqlCommand(query, conn);
+                rda = new SqlDataAdapter();
+
+                rda.UpdateCommand = new SqlCommand(query, conn);
+                rda.UpdateCommand.ExecuteNonQuery();
+
+                output = "Query Complete";
+
+
+            }
+            catch (Exception e)
+            {
+                output = "";
+                output = output + e.Message;
+
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+        }
+
+        // DELETE: api/Employee/5
+        public void Delete(int id)
+        {
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd;
+            SqlDataAdapter rda;
+            String query;
+
+            string output = "";
+
+            try
+            {
+                conn.Open();
+
+
+                query = "delete employee where staffid=" + id;
+                cmd = new SqlCommand(query, conn);
+                rda = new SqlDataAdapter();
+
+                rda.DeleteCommand = new SqlCommand(query, conn);
+                rda.DeleteCommand.ExecuteNonQuery();
+
+                output = "Employee Deletion Complete";
+
+
+            }
+            catch (Exception e)
+            {
+                output = "";
+                output = output + e.Message;
+
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
--- Week11/ExampleWebApi/Controllers/ValuesController.cs
+++ Week11/ExampleWebApi/Controllers/ValuesController.cs
@@ -9,31 +9,36 @@ namespace ExampleWebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+        static string testvalue = "Not set yet";
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", testvalue };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value" + id;
+            return testvalue + " " + id;
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            testvalue = "post " + value;
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            testvalue = "put " + value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            testvalue = "delete " + id;
         }
     }
 }
